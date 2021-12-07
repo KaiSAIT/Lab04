651593e9ef1727c2e6ae63881fc23d7af4b67cbd
849533 <849533@SAIT227692IT.ACDM.DS.SAIT.CA>
2021-12-06 19:09:51 -0700

    NoteServlet.java -updated NoteServlet.java file to import the java bean file

--- Lab04_SimpleNoteKeeper/src/java/servlets/NoteServlet.java
+++ Lab04_SimpleNoteKeeper/src/java/servlets/NoteServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Note;
 
 public class NoteServlet extends HttpServlet {
 
